d957cb3942f53b14554a42250e84ada287d80a3f
kriti kumari <123kritikumari@gmail.com>
2026-03-24 15:15:39 +0530

    microfrontend & microservices project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className='bg-red-200'>App</div>
+      <button className='btn'>Click</button>
+    </div>
+  )
+}
+
+export default App
