e3062bc0c3b9b7c43b05c9503dbe533d5e6fb8d7
kriti kumari <123kritikumari@gmail.com>
2026-04-16 16:43:50 +0530

    changes

--- backend/src/auth/app.js
+++ backend/src/auth/app.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import morgan from 'morgan'
-import cors from "cors"
 import cookieParser from "cookie-parser"
 import authRouter from './routes/auth.routes.js'
 const app = express()
@@ -8,11 +7,6 @@ const app = express()
 app.use(express.json())
 app.use(morgan("dev"))
 app.use(cookieParser())
-
-app.use(cors({
-  origin: "http://localhost:5173", //Access-Control-Allow-Origin: http://localhost:5173
-  credentials: true                //Access-Control-Allow-Credentials: true
-}));
 
  //app.auth is the prefix of api route
  // /app/auth/register is full api route
